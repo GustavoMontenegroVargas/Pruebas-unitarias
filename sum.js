cfe7af0b4c7895c06dda5db577df90928c1541c3
Gustavo Montenegro <c.montenegro2402@gmail.com>
2021-06-28 11:00:54 -0500

    Archivo de la funcion que se exporta

---
+++ sum.js
@@ -0,0 +1,8 @@
+function sum(a,b){
+    return a+b;
+}
+module.exports = sum;/* Queremos exportar esta función en otro archivo
+para poder usarla. Esta es la línea de exportación */
+
+/* Cuando yo solo coloco sum, en este caso, la estoy referenciando
+sin ejecutarla. Sucede igual que con los addEventListeners */
